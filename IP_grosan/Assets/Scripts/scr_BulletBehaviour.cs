b348d96ce7e1597ee25ab35f92f3639428696cc0
RaulGrosan <57448631+RaulGrosan@users.noreply.github.com>
2020-11-23 13:16:15 +0200

    1st commit
    beta ver

---
+++ IP_grosan/Assets/Scripts/scr_BulletBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_BulletBehaviour : MonoBehaviour
+{
+    private Transform bulletTransform;
+    public float speed;
+
+    public GameObject enemyRespawnPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletTransform = GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bulletTransform.position += Vector3.up * speed;
+
+        if(bulletTransform.position.y > 6)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+            Vector3 enemyRespawnPosition = new Vector3(Random.Range(-9,9), 6, 0);
+            Instantiate(enemyRespawnPrefab, enemyRespawnPosition, Quaternion.identity);
+            Instantiate(enemyRespawnPrefab, enemyRespawnPosition, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ IP_grosan/Assets/Scripts/scr_PlayerBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_PlayerBehaviour : MonoBehaviour
+{
+    //movemint
+    private Transform playerTransform;
+    public float speed;
+
+    //bullet related stuff
+    public GameObject bulletFired;
+    public float fireRate;
+    private float nextFire;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(moveH, 0, 0);
+        playerTransform.position += movement * speed;
+
+        if (Input.GetButton("Jump") && Time.time > nextFire)
+        {
+            Instantiate(bulletFired, playerTransform.position, playerTransform.rotation);
+            nextFire = Time.time + fireRate;
+        }
+    }
+}
